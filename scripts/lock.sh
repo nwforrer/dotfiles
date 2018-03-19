07de757044eadfa05d718fc0715103f9157fa705
Nicholas Forrer <nforrer@redhat.com>
2018-03-19 12:23:31 -0400

    Set key press color in i3lock script.

--- scripts/lock.sh
+++ scripts/lock.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-i3lock -i ~/Pictures/wallpaper -tk --indicator --datestr="" --timestr="%H:%M" --ringcolor=282828ff --datecolor=ebdbb2ff --timecolor=ebdbb2ff 
+i3lock -i ~/Pictures/wallpaper -tk --indicator --datestr="" --timestr="%H:%M" --ringcolor=282828ff --datecolor=ebdbb2ff --timecolor=ebdbb2ff --keyhlcolor=ebdbb2ff
