3937b61b67c6da48a2a0279c3f087ec48c96b426
Nicholas Forrer <nforrer@redhat.com>
2018-10-01 14:16:09 -0400

    Pause dunst notifications while screen is locked.

--- scripts/lock.sh
+++ scripts/lock.sh
@@ -1,2 +1,9 @@
 #!/bin/sh
-i3lock -i ~/Pictures/wallpaper -tk --indicator --datestr="" --timestr="%H:%M" --ringcolor=282828ff --datecolor=ebdbb2ff --timecolor=ebdbb2ff --keyhlcolor=ebdbb2ff
+
+#suspend notification display
+pkill -u "$USER" -USR1 dunst
+
+i3lock -n -i ~/Pictures/wallpaper -tk --indicator --datestr="" --timestr="%H:%M" --ringcolor=282828ff --datecolor=ebdbb2ff --timecolor=ebdbb2ff --keyhlcolor=ebdbb2ff
+
+#resume notification display
+pkill -u "$USER" -USR2 dunst
